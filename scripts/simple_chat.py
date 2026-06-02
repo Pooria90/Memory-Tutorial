b5c2276d7a26f6f183f952b80a6ba240d8a579b4
Pooria Ashrafian <pooria@railtown.ai>
2026-06-01 18:43:29 -0700

    Add simple chat agent implementation for user interaction

---
+++ scripts/simple_chat.py
@@ -0,0 +1,52 @@
+import asyncio
+import railtracks as rt
+
+from rich import print
+from railtracks.llm import (
+    MessageHistory,
+    UserMessage,
+    AssistantMessage
+)
+
+from amygdala.agent import SimpleAgent
+
+
+_opener = """
+Welcome! You can start chatting.
+(type `quit` to exit)\n
+User:
+"""
+
+_closer = """\nAssistant:
+Goodbye!
+"""
+
+
+async def run_agent():
+    user_msg: str = input(_opener)
+    resp = None
+    msg_history = MessageHistory()
+
+    while True:
+        msg_history.append(
+            UserMessage(user_msg)
+        )
+        if user_msg.strip().lower() == "quit":
+            print(_closer)
+            break
+        resp = await rt.call(SimpleAgent, msg_history)
+        msg_history.append(
+            AssistantMessage(resp.content)
+        )
+
+        print(f"\nAssistant:\n{resp.content}")
+
+        user_msg = input("\nUser:\n")
+
+    return resp
+
+
+
+if __name__ == "__main__":
+    resp = asyncio.run(run_agent())
+    print(f"\nMessage History:\n{resp.message_history}")
